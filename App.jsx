5d53b215acf64973f7df23fb1033c22744c92ef4
Honey <yurjune@naver.com>
2021-09-02 23:02:21 +0900

    style: changed style

--- App.jsx
+++ App.jsx
@@ -10,13 +10,16 @@ const App = () => {
 
   const createPlan = (e) => {
     e.preventDefault();
+    if (!value) {
+      return alert('내용을 입력해주세요!');
+    }
     setTodo((prevTodo) => {
       return [...prevTodo, value];
     });
     setValue('');
   };
 
-  const removePlan = (v) => {
+  const deletePlan = (v) => {
     setTodo((prevTodo) => {
       const data = [...prevTodo];
       const delete_target = data.indexOf(v);
@@ -25,29 +28,32 @@ const App = () => {
     });
   }
 
-  const onClickBtn = (v) => (e) => {
+  const clickDelBtn = (v) => (e) => {
     console.log(v);
-    removePlan(v);
+    deletePlan(v);
+  };
+
+  const clickFinishBtn = (v) => (e) => {
   };
 
   return(
-    <>
+    <div id='container'>
       <form onSubmit={createPlan}>
-        <input type="text" value={value} onChange={onChangeInput} />
-        <button>버튼</button>
+        <input type="text" value={value} onChange={onChangeInput} placeholder='할 일을 적어보세요'/>
+        <button id="create-btn">등록</button>
       </form>
       <div>
         {todo.map((v, i) => {
           return (
-            <>
-              <li>{v}</li>
-              <button onClick={onClickBtn(v)}>삭제</button>
-              <br/>
-            </>
+            <div id='box'>
+              <li id='list'>{v}</li>
+              <button id='finish-btn' onClick={clickDelBtn(v)}>o</button>
+              <button id='del-btn' onClick={clickDelBtn(v)}>x</button>
+            </div>
           )
         })}
       </div>
-    </>
+    </div>
   )
 }
 
